e6e1a6278ec8127a937b5989fa7ee3dd3781998a
Mike Combs <mike@combsnet.com>
2019-05-16 00:51:18 -0400

    feat: resizes face art, adds white rounded border

--- img-to-card.py
+++ img-to-card.py
@@ -3,6 +3,11 @@
 import os
 from PIL import Image
 
+deck_path = '/Users/mikec/Documents/gaming/DriveThruRPG/Just Insert Imagination/Pirate cards, renamed'
+dpi = 70
+card_size = (2.5 * dpi, 3.5 * dpi)
+shadow = 7
+
 
 def getCardList(deck_path):
     """Return list of card file names.
@@ -22,18 +27,24 @@ def getCardList(deck_path):
 
 def cardify(cards, card_size, shadow):
     """Resize, apply white borders and shadow."""
+
+    card_l1 = Image.open('00 poker card border.png')
+    card_src = Image.open(os.path.join(deck_path, cards[0]))
+    card_src = card_src.resize((151, 225))  # fit inside inner border
+    # place inside on white card bg
+    card_l1.paste(card_src, (12, 10))
+    # round corners of inside image
+    card_l1.paste(Image.open('00 poker card border.png'), (0,0), Image.open('00 poker card border.png'))
+    card_l1.show()
     return
 
 def main():
-    deck_path = '/Users/mikec/Documents/gaming/DriveThruRPG/Just Insert Imagination/Pirate cards, renamed'
-    dpi = 70
-    card_size = (2.5 * dpi, 3.5 * dpi)
-    shadow = 7
-
     cards = getCardList(deck_path)
     print(cards)
     c1 = Image.open(os.path.join(deck_path, cards[0]))
     c1.show()
+
+    cardify(cards, card_size, 0)
 
 if __name__ == "__main__":
     main()
